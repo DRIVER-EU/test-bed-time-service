10a7ca98fe29da3cead9f9e20efcc604e679be2e
Erik Vullings <erik.vullings@tno.nl>
2023-11-14 11:37:22 +0100

    Add chime sound when a new billboard message arrives

--- packages/gui/src/components/billboard.ts
+++ packages/gui/src/components/billboard.ts
@@ -1,10 +1,25 @@
 import m from 'mithril';
 import { InfoMsg, InfoMsgType, SocketChannels } from '../models';
 import { SocketService } from '../services/socket-service';
+import chimeSrc from '../assets/chime.mp3';
+import { render } from 'slimdown-js';
 
 export const Billboard = () => {
   let headline: string = '';
   let description: string = '';
+  let chimeSound: HTMLAudioElement;
+
+  const playChime = () => {
+    console.log('PLAY CHIME')
+    if (!chimeSound) return;
+    // Check if the audio is already playing and stop it if it is
+    if (!chimeSound.paused) {
+      chimeSound.pause();
+      chimeSound.currentTime = 0;
+    }
+    // Play the chime sound
+    chimeSound.play();
+  }
 
   return {
     oninit: () => {
@@ -16,15 +31,18 @@ export const Billboard = () => {
         } else {
           headline = msg.headline;
           description = msg.description;
+          playChime();
         }
         m.redraw();
       });
     },
     view: () => {
-      return m(
-        '#billboard.billboard',
-        m('.message', { className: 'center' }, [m('h1', headline), m('div', description)])
-      );
+      return [
+        m('audio#chimeSound', { src: chimeSrc, style: 'display: none;', oncreate: ({ dom }) => chimeSound = dom as HTMLAudioElement }),
+        m(
+          '#billboard.billboard',
+          m('.message', { className: 'center' }, [m('h1', m.trust(render(headline, true))), m('div', m.trust(render(description, true)))])
+        )];
     },
   };
 };
--- packages/gui/src/types/declarations.d.ts
+++ packages/gui/src/types/declarations.d.ts
@@ -2,3 +2,8 @@ declare module "*.png" {
   const foo: any;
   export default foo;
 }
+
+declare module "*.mp3" {
+  const foo: any;
+  export default foo;
+}
